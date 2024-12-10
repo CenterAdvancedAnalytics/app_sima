fd84e2d5c2b4104150cdcb2fb41aa199aa19984d
geraldc <geraldcamposm10@gmail.com>
2024-12-10 01:49:48 -0500

    Update

--- app/update_tables.py
+++ app/update_tables.py
@@ -16,17 +16,16 @@ import streamlit as st
 # DB_PORT = os.getenv('DB_PORT')
 # DB_NAME = os.getenv('DB_NAME')
 
-DB_USER = st.secrets["DB_USER"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
-
-# URL y motor de conexión
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 def update_data():
+    DB_USER = st.secrets["DB_USER"]
+    DB_PASSWORD = st.secrets["DB_PASSWORD"]
+    DB_HOST = st.secrets["DB_HOST"]
+    DB_PORT = st.secrets["DB_PORT"]
+    DB_NAME = st.secrets["DB_NAME"]
+
+    # URL y motor de conexión
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
     queries = {
         # Queries de cocteles
         "base": {
